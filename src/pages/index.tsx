c237f3e028ae47490d897f89e3ea002911322efd
Renan Dantas <reehdantascarvalho@gmail.com>
2021-02-23 20:35:35 -0300

    feat: adding favicon and page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,15 @@ import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title> Inicio | Moveit </title>
+      </Head>
       <ExperienceBar />
-
       <section>
         <div>
           <Profile />
