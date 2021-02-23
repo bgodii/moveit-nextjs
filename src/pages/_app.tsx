f72ac7c9208e23ad5ad82c9a9db58090d405a774
Renan Dantas <reehdantascarvalho@gmail.com>
2021-02-23 19:51:01 -0300

    First Commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/global.css'
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp
